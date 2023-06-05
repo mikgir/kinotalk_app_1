1e72761018d0234ddc7ee23751472a5921c341ba
a_z <azdvd@mail.ru>
2023-06-05 18:10:21 +0300

    add search livewire 2

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,10 +6,12 @@ use App\Traits\Commentable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class News extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     use Commentable;
 
     protected $table = 'news';
